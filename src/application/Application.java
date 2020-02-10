101d38be10935dd81c2ecf96101e2d3fca7be149
NathanDuPont <nathan.dupont01@gmail.com>
2020-02-09 20:12:50 -0600

    Created application structure, and example JSON response

---
+++ src/application/Application.java
@@ -0,0 +1,19 @@
+/*
+ * Course: SE2811-051
+ * Winter 2019-2020
+ * Application - Final Presentation
+ * Name: Milan Kablar, Jonas Cira, Nathan DuPont
+ * Created: 2/9/2020
+ */
+
+package application;
+
+/**
+ * SE2811-051 Winter 2019-2020
+ * Application purpose: // TODO
+ *
+ * @author // TODO
+ * @version created on 2/9/2020 at 8:11 PM
+ */
+public class Application {
+}
