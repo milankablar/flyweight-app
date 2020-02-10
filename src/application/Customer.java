454c82dadf59c7d06f829b0351af31b1983b99f8
NathanDuPont <nathan.dupont01@gmail.com>
2020-02-09 20:58:09 -0600

    Added UML and customer construction framework

---
+++ src/application/Customer.java
@@ -0,0 +1,19 @@
+/*
+ * Course: SE2811-051
+ * Winter 2019-2020
+ * CustomerFactory - Final Presentation
+ * Name: Milan Kablar, Jonas Cira, Nathan DuPont
+ * Created: 2/9/2020
+ */
+
+package application;
+
+/**
+ * SE2811-051 Winter 2019-2020
+ * Customer purpose:
+ *
+ * @author dupontn
+ * @version created on 2/9/2020 at 8:40 PM
+ */
+public class Customer {
+}
